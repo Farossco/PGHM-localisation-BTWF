c4194ad6871b66c195716910644492af38560484
Farès <faresticha@gmail.com>
2021-02-23 17:00:48 +0100

    Create code for peripheral detection using Wi-Fi
    Code based on the Friend Detector by Ricardo Oliviera (https://github.com/RicardoOliveira/FriendDetector)

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,138 @@
 /*
-    This sketch demonstrates how to scan WiFi networks.
-    The API is almost the same as with the WiFi Shield library,
-    the most obvious difference being the different file you need to include:
+ * Friend Detector by Ricardo Oliveira, modified by Farossco 2/23/2021
+ *
+ * A Node MCU microcontroller + mini bread board + 4 pin RGB LED to detect when devices belonging to a target are nearby.
+ *
+ *
+ * The function of this code is to read nearby Wi-Fi traffic in the form of packets. These packets are compared
+ * to a list of MAC addresses we wish to track, and if the MAC address of a packet matches one on the list, we turn
+ * on a colored LED that is linked to the user owning the device. For example, when my roommate comes home, the
+ * transmissions from his phone will be detected and cause the blue LED to turn on until his phone is no longer detected.
+ * It can detect more than one phone at a time, meaning if my phone (red) and my roommate's phone (blue) are both home,
+ * the LED will show purple.
  */
-#include "ESP8266WiFi.h"
 
-void setup()
+#include <Arduino.h>
+#include "esppl_functions.h"
+
+/* If you want to track a specific address, it can be put in the friendMac list */
+#define FRIEND_LIST_SIZE   0
+#define DETECTED_LIST_SIZE 200
+#define RSSI_THRESHOLD     -40
+#define PRINT_NEARBY       false
+
+static int detectedListIndex = 0;
+
+/*
+ * This is your friend's MAC address list
+ * Format it by taking the mac address aa:bb:cc:dd:ee:ff
+ * and converting it to 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff
+ */
+uint8_t friendMac[FRIEND_LIST_SIZE][ESPPL_MAC_LEN] = { };
+
+/* This stores the MAC addresses we already detected to avoid printing them more than once*/
+uint8_t detectedmac[DETECTED_LIST_SIZE][ESPPL_MAC_LEN];
+
+/*
+ * This is your friend's name list
+ * put them in the same order as the MAC addresses
+ */
+String friendname[FRIEND_LIST_SIZE] = { };
+
+bool maccmp (uint8_t * mac1, uint8_t * mac2)
 {
-	Serial.begin(115200);
-
-	// Set WiFi to station mode and disconnect from an AP if it was previously connected
-	WiFi.mode(WIFI_STA);
-	WiFi.disconnect();
-	delay(100);
-
-	Serial.println("Setup done");
-}
-
-void loop()
-{
-	Serial.println("scan start");
-
-	// WiFi.scanNetworks will return the number of networks found
-	int n = WiFi.scanNetworks();
-	Serial.println("scan done");
-	if (n == 0) {
-		Serial.println("no networks found");
-	} else {
-		Serial.print(n);
-		Serial.println(" networks found");
-		for (int i = 0; i < n; ++i) {
-			// Print SSID and RSSI for each network found
-			Serial.print(i + 1);
-			Serial.print(": ");
-			Serial.print(WiFi.SSID(i));
-			Serial.print(" (");
-			Serial.print(WiFi.RSSI(i));
-			Serial.print(")");
-			Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*");
-			delay(10);
+	for (int i = 0; i < ESPPL_MAC_LEN; i++)
+	{
+		if (mac1[i] != mac2[i])
+		{
+			return false;
 		}
 	}
-	Serial.println("");
+	return true;
+}
 
-	// Wait a bit before scanning again
-	delay(5000);
+void maccpy (uint8_t * macS, uint8_t * macD)
+{
+	for (int i = 0; i < ESPPL_MAC_LEN; i++)
+		macD[i] = macS[i];
+}
+
+void displayDevice (esppl_frame_info * info)
+{
+	for (int i = 0; i < ESPPL_MAC_LEN; i++)
+		Serial.printf ("%02X", info->sourceaddr[i]);
+
+	Serial.printf (" - ");
+
+	for (int i = 0; i < ESPPL_MAC_LEN; i++)
+		Serial.printf ("%02X", info->receiveraddr[i]);
+
+	Serial.printf (" (RSSI : %d dBm)\n", info->rssi);
+}
+
+void cb (esppl_frame_info * info)
+{
+	bool newMac = true;
+
+	for (int i = 0; i < detectedListIndex; i++)
+		if (maccmp (info->sourceaddr, detectedmac[i]))
+			newMac = false;
+
+	/* Detecting and printing close (RSSI > RSSI_THRESHOLD) devices as many times as we see them*/
+	if (PRINT_NEARBY && info->rssi > RSSI_THRESHOLD)
+	{
+		Serial.printf ("[Near] ");
+
+		displayDevice (info);
+	}
+
+	/* Printing newly detected addresses, these addresses are printed once only */
+	if (newMac)
+	{
+		if (detectedListIndex == DETECTED_LIST_SIZE)
+		{
+			printf ("Detected list size full\n");
+		}
+		else
+		{
+			Serial.printf ("[New ] ");
+
+			displayDevice (info);
+
+			maccpy (info->sourceaddr, detectedmac[detectedListIndex]);
+			detectedListIndex++;
+		}
+	}
+
+	/* Prints if a friend device is detected */
+	for (int i = 0; i < FRIEND_LIST_SIZE; i++)
+	{
+		if (maccmp (info->sourceaddr, friendMac[i]) || maccmp (info->receiveraddr, friendMac[i]))
+		{
+			Serial.printf ("\n%s is here! :)", friendname[i].c_str());
+		}
+	}
+} // cb
+
+// start variables package - Skickar 2018 hardware LED for NodeMCU on mini breadboard //
+void setup ()
+{
+	delay (500);
+	Serial.begin (115200);
+	esppl_init (cb);
+}
+
+void loop ()
+{
+	// I didn't write this part but it sure looks fancy.
+	esppl_sniffing_start();
+	while (true)
+	{
+		for (int i = ESPPL_CHANNEL_MIN; i <= ESPPL_CHANNEL_MAX; i++)
+		{
+			esppl_set_channel (i);
+			while (esppl_process_frames())
+			{ }
+		}
+	}
 }
